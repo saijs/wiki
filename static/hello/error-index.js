7b256e1cdd9c5989036c7b66e6bb8a6ade87b83d
hotoo <hotoo.cn@gmail.com>
2013-04-03 15:20:11 +0800

    A: init gh-pages

---
+++ static/hello/error-index.js
@@ -0,0 +1,5 @@
+define({
+  "客户端没有所需的特权。": "the-client-does-not-have-the-required-privileges",
+  "Array length must be a finite positive integer": "array-length-must-be-a-finite-positive-integer"
+  "动态链接库(DLL)初始化例程失败。":""
+});
